f5e5b810d54625634f07d7be8b61c3057b3f31ac
Julio Lopez <JL223706@fi365.ort.edu.uy>
2026-01-29 18:51:57 -0300

    add UC crearAppointment

---
+++ patrones/src/main/java/Services/FachadaSistema.java
@@ -0,0 +1,34 @@
+package Services;
+
+import java.time.LocalDateTime;
+
+import Persistencia.RepositorioPacientes;
+import Persistencia.RepositorioProfesionales;
+import Persistencia.RepositorioTurnos;
+
+public class FachadaSistema {
+    //inicializacion de los repositorios
+    private RepositorioPacientes repoPacientes= new RepositorioPacientes();
+    private RepositorioProfesionales repoProfesionales = new RepositorioProfesionales();
+    private RepositorioTurnos repoTurnos = new RepositorioTurnos();
+
+    // inicializacion de los servicios
+    private ServicioTurno servicioTurno = new ServicioTurno(repoPacientes, repoProfesionales, repoTurnos);
+    private ServicioPaciente servicioPaciente = new ServicioPaciente();
+    private ServicioProfesional servicioProfesional = new ServicioProfesional();
+
+    // // singleton pattern
+    // private static FachadaSistema fachada = null;
+
+    // public static FachadaSistema getFachada() {
+    //     if (fachada == null) {
+    //         fachada = new FachadaSistema();
+    //     }
+    //     return fachada;
+    // }
+
+    public void crearTurno(int numSocio, int idMedico, LocalDateTime fechaHora) {
+        servicioTurno.crearTurno(numSocio, idMedico, fechaHora);
+    }
+
+}
---
+++ patrones/src/main/java/Services/ServicioTurno.java
@@ -0,0 +1,67 @@
+package Services;
+
+import Dominio.Turno;
+import Dominio.Paciente;
+import Dominio.Profesional;
+
+import interfaces.PoliticasDeAtencion.IPoliticaAtencion;
+import interfaces.PoliticasDeAtencion.PoliticaParticular;
+import interfaces.PoliticasDeAtencion.PoliticaMutualista;
+import interfaces.PoliticasDeAtencion.PoliticaUrgencia;
+
+import Exceptions.PacienteNoEncontradoException;
+import Exceptions.ProfesionalNoDisponibleException;
+import Exceptions.ProfesionalNoEncontradoException;
+
+import Persistencia.RepositorioTurnos;
+import Persistencia.RepositorioPacientes;
+import Persistencia.RepositorioProfesionales;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ServicioTurno {
+    // Tiene la coleccion de todos los turnos en el sistema
+    // en caso de no encontrar el turno, devuelve TurnoNoEncontradoException
+    private List<IPoliticaAtencion> politicasAtencion;
+    private RepositorioPacientes repoPacientes;
+    private RepositorioProfesionales repoProfesionales;
+    private RepositorioTurnos repoTurnos;
+
+    
+    public ServicioTurno(RepositorioPacientes p_repoPaciente, RepositorioProfesionales p_repoProfesionales,
+            RepositorioTurnos p_repoTurnos) {
+        this.repoPacientes = p_repoPaciente;
+        this.repoProfesionales = p_repoProfesionales;
+        this.repoTurnos = p_repoTurnos;
+        this.politicasAtencion = List.of(new PoliticaParticular(), new PoliticaMutualista(), new PoliticaUrgencia());
+    }
+
+    public Turno crearTurno(int numSocio, int IdProfesional, LocalDateTime fechaHora) {
+        // Lógica para crear un turno
+        // Verificar si el paciente y profesional existen
+        Paciente paciente = repoPacientes.buscarPorId(numSocio).orElseThrow(
+                () -> new PacienteNoEncontradoException("No se encontro el paciente con id: " + numSocio));
+
+        Profesional profesional = repoProfesionales.buscarPorId(IdProfesional).orElseThrow(
+                () -> new ProfesionalNoEncontradoException("No se encontro el profesional con id: " + IdProfesional));
+
+        // Verificar si el profesional está disponible en la fecha y hora solicitada
+        if (!profesional.estaDisponible(fechaHora)) {
+            throw new ProfesionalNoDisponibleException(
+                    "El profesional con id: " + IdProfesional + " no esta disponible en la fecha y hora solicitada.");
+        }
+
+        // aplica la politica de atencion
+        for (IPoliticaAtencion politica : politicasAtencion) {
+            politica.validarCreacionTurno(paciente, profesional, fechaHora);
+        }
+
+        // Crear y guardar el turno en el repositorio
+        Turno nuevoTurno = new Turno(paciente, profesional, fechaHora);
+        repoTurnos.guardar(nuevoTurno);
+        // Devolver el turno creado
+        return nuevoTurno;
+    }
+
+}
