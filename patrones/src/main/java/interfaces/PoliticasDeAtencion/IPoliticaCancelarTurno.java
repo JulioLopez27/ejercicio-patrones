6fcf28ade219657e3643ac3cc22ceb3cc7c0abd9
Julio Lopez <JL223706@fi365.ort.edu.uy>
2026-01-31 05:42:23 -0300

    add cancel policy interface/logic

---
+++ patrones/src/main/java/interfaces/PoliticasDeAtencion/IPoliticaCancelarTurno.java
@@ -0,0 +1,7 @@
+package interfaces.PoliticasDeAtencion;
+
+import Dominio.Turno;
+
+public interface IPoliticaCancelarTurno {
+    void validarCancelacionTurno(Turno turno);
+}
---
+++ patrones/src/main/java/interfaces/PoliticasDeAtencion/PoliticaAntecipacionMinima.java
@@ -0,0 +1,17 @@
+package interfaces.PoliticasDeAtencion;
+
+import Dominio.Turno;
+import Exceptions.CancelacionInvalidaException;
+
+import java.time.LocalDateTime;
+
+public class PoliticaAntecipacionMinima implements IPoliticaCancelarTurno {
+
+    public void validarCancelacionTurno(Turno turno) {
+        if (turno.getFechaYHora().minusHours(24).isBefore(LocalDateTime.now())) {
+            throw new CancelacionInvalidaException(
+                    "No se puede cancelar el turno con menos de 24 horas de anticipación");
+        }
+    }
+
+}
