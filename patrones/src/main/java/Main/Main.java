1ea7ab9cb4d7b4ac49cec484fd7f27e7f4036b99
Julio Lopez <JL223706@fi365.ort.edu.uy>
2026-02-09 13:16:09 -0300

    console ui

--- patrones/src/main/java/Main/Main.java
+++ patrones/src/main/java/Main/Main.java
@@ -1,8 +1,63 @@
 package Main;
 
+import java.util.Scanner;
+
+import Services.FachadaSistema;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FachadaSistema fachada = new FachadaSistema();
+        Scanner sc = new Scanner(System.in);
+
+        boolean salir = false;
+
+        while (!salir) {
+            System.out.println("\n=== MENU ===");
+            System.out.println("1 - Alta Paciente");
+            System.out.println("2 - Alta Profesional");
+            System.out.println("3 - Reservar Turno (pendiente)");
+            System.out.println("4 - Listar Pacientes");
+            System.out.println("5 - Salir");
+            System.out.print("Opcion: ");
+
+            int opcion = sc.nextInt();
+            sc.nextLine(); // limpiar buffer
+
+            switch (opcion) {
+                case 1 -> {
+                    System.out.print("Nombre: ");
+                    String nombre = sc.nextLine();
+                    System.out.print("Direccion: ");
+                    String direccion = sc.nextLine();
+                    System.out.print("Correo: ");
+                    String correo = sc.nextLine();
+                    System.out.print("DNI: ");
+                    //TODO: validar que el dni sea un numero entero
+                    int dni = sc.nextInt();
+                    sc.nextLine(); // limpiar buffer
+
+                    fachada.altaPaciente(dni, nombre, direccion, correo);
+                }
+
+                case 2 -> {
+                    System.out.print("Nombre del profesional: ");
+                    String nombre = sc.nextLine();
+                    // fachada.altaProfesional(nombre);
+                }
+
+                case 3 -> {
+                    System.out.println("Reservar turno - NO IMPLEMENTADO");
+                }
+
+                // case 4 -> fachada.listarPacientes();
+
+                case 5 -> salir = true;
+
+                default -> System.out.println("Opcion invalida");
+            }
+        }
+
+        sc.close();
     }
-    
+
 }
