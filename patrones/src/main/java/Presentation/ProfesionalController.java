26fed1a2efd310e19b850a2e92546c428f2afad8
Julio Lopez <JL223706@fi365.ort.edu.uy>
2026-02-11 02:37:31 -0300

    move from main add logic

--- patrones/src/main/java/Presentation/ProfesionalController.java
+++ patrones/src/main/java/Presentation/ProfesionalController.java
@@ -1,15 +1,19 @@
 package Presentation;
 
 import java.util.List;
+import java.util.Scanner;
 
 import Dominio.TipoEspecialidad;
 import Services.FachadaSistema;
 
 public class ProfesionalController {
-    private FachadaSistema fachada = new FachadaSistema();
 
-    public void altaProfesional(int dni, String nombre, String correo, TipoEspecialidad tipo) {
-        fachada.altaProfesional(dni, nombre, correo, tipo);
+    private final FachadaSistema fachada;
+    private final Scanner sc;
+
+    public ProfesionalController(FachadaSistema p_fachada, Scanner p_sc) {
+        this.fachada = p_fachada;
+        this.sc = p_sc;
     }
 
     public List<TipoEspecialidad> mostrarTiposEspecialidad() {
@@ -21,4 +25,31 @@ public class ProfesionalController {
         }
         return tipos;
     }
+
+    public void altaProfesional() {
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("Correo: ");
+        String correo = sc.nextLine();
+        System.out.print("Documento: ");
+        int documento = sc.nextInt();
+        sc.nextLine(); // limpiar buffer
+
+        List<TipoEspecialidad> tiposEspecialidad = this.mostrarTiposEspecialidad();
+        TipoEspecialidad especialidad = null;
+
+        while (especialidad == null) {
+            System.out.println("Seleccione el tipo de especialidad:");
+            int opcion = sc.nextInt();
+            sc.nextLine();
+
+            if (opcion >= 1 && opcion <= tiposEspecialidad.size()) {
+                especialidad = tiposEspecialidad.get(opcion - 1);
+            } else {
+                System.out.println("Opcion invalida, intente nuevamente.");
+            }
+        }
+        fachada.altaProfesional(documento, nombre, correo, especialidad);
+    }
+
 }
