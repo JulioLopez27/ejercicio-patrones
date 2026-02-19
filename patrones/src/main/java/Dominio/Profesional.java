cb9d1ed5659c9d096c6763363a605f801ab625ab
Julio Lopez <JL223706@fi365.ort.edu.uy>
2026-02-19 16:41:50 -0300

    --

--- patrones/src/main/java/Dominio/Profesional.java
+++ patrones/src/main/java/Dominio/Profesional.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 
 public class Profesional {
     private static int idCounter = 1;
-    private int idProfesional,dni;
-    private String nombre,correo;
+    private int idProfesional, dni;
+    private String nombre, correo;
     private TipoEspecialidad tipo;
     protected List<Turno> turnos;
 
-    public Profesional(int p_dni,String p_nombre, String p_correo, TipoEspecialidad p_tipo) {
+    public Profesional(int p_dni, String p_nombre, String p_correo, TipoEspecialidad p_tipo) {
         this.idProfesional = idCounter++;
         this.dni = p_dni;
         this.nombre = p_nombre;
@@ -29,18 +29,23 @@ public class Profesional {
         return true;
     }
 
-    public void verificiarDisponibilidad(){
+    public void verificiarDisponibilidad() {
 
     }
 
-    public void asignarTurno(){
-
+    public void asignarTurno(Turno t) {
+        this.turnos.add(t);
     }
 
     public int getId() {
         return this.idProfesional;
     }
-@Override
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    @Override
     public String toString() {
         return "Profesional { " +
                 "idProfesional=" + idProfesional +
