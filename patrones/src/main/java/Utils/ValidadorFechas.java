36eb2c26bfb8da2245c930114a6b4c88cb6ec795
Julio Lopez <JL223706@fi365.ort.edu.uy>
2026-02-19 04:01:41 -0300

    Add aux for dates

---
+++ patrones/src/main/java/Utils/ValidadorFechas.java
@@ -0,0 +1,17 @@
+package Utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class ValidadorFechas {
+    private static final DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+    public static LocalDateTime leerFecha(String input) {
+        try {
+            return LocalDateTime.parse(input, formato);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Formato de fecha inválido. Use el formato: dd-MM-yyyy HH:mm");
+        }
+    }
+}
