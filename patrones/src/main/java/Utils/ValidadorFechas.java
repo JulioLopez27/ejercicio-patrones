a419d76cfe70ed6bc8d72a1c21754dbd33b6460c
Julio <julioneo95@hotmail.com>
2026-03-09 22:18:54 -0300

    Constructor/err visualization

--- patrones/src/main/java/Utils/ValidadorFechas.java
+++ patrones/src/main/java/Utils/ValidadorFechas.java
@@ -7,11 +7,13 @@ import java.time.format.DateTimeParseException;
 public class ValidadorFechas {
     private static final DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
+    public ValidadorFechas() {}
+
     public static LocalDateTime leerFecha(String input) {
         try {
             return LocalDateTime.parse(input, formato);
         } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("Formato de fecha inválido. Use el formato: dd-MM-yyyy HH:mm");
+            throw new IllegalArgumentException("Formato de fecha inválido. Use el formato: dd-MM-yyyy HH:mm", e);
         }
     }
 }
