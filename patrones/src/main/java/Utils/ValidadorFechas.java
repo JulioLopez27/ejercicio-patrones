8fccb9c063b2ae97b64c0cdc73261851887a3e00
Julio <julioneo95@hotmail.com>
2026-03-13 15:56:12 -0300

    improve logic/eliminate bug

--- patrones/src/main/java/Utils/ValidadorFechas.java
+++ patrones/src/main/java/Utils/ValidadorFechas.java
@@ -3,15 +3,23 @@ package Utils;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 
 public class ValidadorFechas {
     private static final DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
-    private ValidadorFechas() {}
+    private ValidadorFechas() {
+    }
 
     public static LocalDateTime leerFecha(String input) {
         try {
-            return LocalDateTime.parse(input, formato);
+            LocalDateTime fecha = LocalDateTime.parse(input, formato);
+            fecha = fecha.truncatedTo(ChronoUnit.MINUTES);
+            if(fecha.isBefore(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES)))
+            {
+                throw new IllegalArgumentException("No se puede ingresar fechas pasadas. ");
+            }
+            return fecha;
         } catch (DateTimeParseException e) {
             throw new IllegalArgumentException("Formato de fecha inválido. Use el formato: dd-MM-yyyy HH:mm", e);
         }
