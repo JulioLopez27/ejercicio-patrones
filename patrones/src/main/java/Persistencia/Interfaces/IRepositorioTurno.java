6579488a9374fe5612edf63e91d56667a9c02a60
Julio <julioneo95@hotmail.com>
2026-03-13 15:55:22 -0300

    Add: get patient appointments

--- patrones/src/main/java/Persistencia/Interfaces/IRepositorioTurno.java
+++ patrones/src/main/java/Persistencia/Interfaces/IRepositorioTurno.java
@@ -1,9 +1,12 @@
 package Persistencia.Interfaces;
 
+import java.util.List;
 import java.util.Optional;
 
 import Dominio.Turno;
 
 public interface IRepositorioTurno extends IRepositorio<Turno> {
-Optional<Turno> buscarPorId(int idTurno); 
+    Optional<Turno> buscarPorId(int idTurno);
+
+    List<Turno> obtenerTurnosDelPaciente(int idPaciente);
 }
