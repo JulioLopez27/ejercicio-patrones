8962d001d3e57721adc24782c57c89d1b3a65640
Julio <julioneo95@hotmail.com>
2026-03-13 15:44:00 -0300

    Add:Setter/ Date validator/ Time per appointment/auto-assign prof-turno

--- patrones/src/main/java/Dominio/Turno.java
+++ patrones/src/main/java/Dominio/Turno.java
@@ -4,12 +4,13 @@ import java.time.LocalDateTime;
 
 import Observer.Observable;
 
-
 public class Turno extends Observable {
     private static int contadorId = 1;
     private int idTurno;
     private EstadoTurno estado;
     private LocalDateTime fechaYHora;
+    // TODO: no olvidar: cambiar a dinamico a futuro.
+    private static int DURACION_TURNO_MINUTOS = 30;
     private Paciente paciente;
     private Profesional profesional;
 
@@ -19,6 +20,9 @@ public class Turno extends Observable {
         this.paciente = paciente;
         this.profesional = p_profesional;
         this.idTurno = contadorId++;
+
+        //al crear un turno, este mismo se asigna al profesional seleccionado(automatico)
+        this.profesional.asignarTurno(this);
     }
 
     // Comportamientos del turno
@@ -45,8 +49,16 @@ public class Turno extends Observable {
         return this.estado;
     }
 
-    public LocalDateTime getFechaYHora() {
+    public LocalDateTime getFechaInicio() {
         return fechaYHora;
+    }
+
+    public LocalDateTime getFechaFin() {
+        return this.fechaYHora.plusMinutes(getDuracionDelTurno());
+    }
+
+    public static int getDuracionDelTurno() {
+        return DURACION_TURNO_MINUTOS;
     }
 
     public Paciente getPaciente() {
@@ -57,18 +69,28 @@ public class Turno extends Observable {
         return this.profesional;
     }
 
+    public void nuevaFecha(LocalDateTime nuevaFecha) {
+        this.modificarPorNuevaFecha(nuevaFecha);
+    }
+
+    private void modificarPorNuevaFecha(LocalDateTime nf) {
+        this.fechaYHora = nf;
+    }
+
+    public boolean seSuperpone(LocalDateTime nuevoInicio, LocalDateTime nuevoFin) {
+        return this.getFechaInicio().isBefore(nuevoFin) && nuevoInicio.isBefore(getFechaFin());
+    }
+
     @Override
     public String toString() {
         return "Turno { " +
+                "id = '" + this.getIdTurno() + '\'' +
                 "paciente = '" + paciente.getNombre() + '\'' +
                 ", profesional = '" + profesional.getNombre() + '\'' +
-                ", fecha = '" + this.getFechaYHora() + '\'' +
+                ", fecha = '" + this.getFechaInicio() + '\'' +
                 ", estado= '" + this.getEstado() +
                 '}';
 
     }
-
- 
-
 
 }
